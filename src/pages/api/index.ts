26918dd6c27959d8d5dcfef6683fd4e76a40630a
Nenad Novakovic <dvlden@users.noreply.github.com>
2022-12-06 21:54:32 +0100

    feat: add api to delete record

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -5,13 +5,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type RequestBody = {
   link: string
+  id: number
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method !== 'POST') {
+  if (req.method && ['GET', 'PUT', 'PATCH'].includes(req.method)) {
     return res.status(405).json({ message: 'Method now allowed' })
   }
 
@@ -19,35 +20,51 @@ export default async function handler(
     return res.status(401).json({ message: 'Unauthorized' })
   }
 
-  if (!isValidBody<RequestBody>(req.body, ['link'])) {
-    return res.status(422).json({ message: 'Unprocessable entity' })
+  // CREATE
+  if (req.method === 'POST') {
+    if (!isValidBody<RequestBody>(req.body, ['link'])) {
+      return res.status(422).json({ message: 'Unprocessable entity' })
+    }
+
+    const link = req.body.link
+
+    if (!isValidLink(link)) {
+      return res.status(422).json({ message: 'Unprocessable entity' })
+    }
+
+    const hid = new Hashids(process.env.HASHID_SALT, 3)
+    const latestEntry = await prisma.entry.findMany({
+      select: {
+        id: true,
+      },
+      orderBy: {
+        id: 'desc',
+      },
+      take: 1,
+    })
+
+    const id = (latestEntry[0]?.id || 0) + 1
+
+    const data = await prisma.entry.create({
+      data: {
+        link,
+        slug: hid.encode(id),
+      },
+    })
+
+    return res.status(200).json(data)
   }
 
-  const link = req.body.link
+  // DELETE
+  if (req.method === 'DELETE') {
+    if (!isValidBody<RequestBody>(req.body, ['link', 'id'])) {
+      return res.status(422).json({ message: 'Unprocessable entity' })
+    }
 
-  if (!isValidLink(link)) {
-    return res.status(422).json({ message: 'Unprocessable entity' })
+    await prisma.entry.delete({
+      where: {
+        id: req.body.id,
+      },
+    })
   }
-
-  const hid = new Hashids(process.env.HASHID_SALT, 3)
-  const latestEntry = await prisma.entry.findMany({
-    select: {
-      id: true,
-    },
-    orderBy: {
-      id: 'desc',
-    },
-    take: 1,
-  })
-
-  const id = (latestEntry[0]?.id || 0) + 1
-
-  const data = await prisma.entry.create({
-    data: {
-      link,
-      slug: hid.encode(id),
-    },
-  })
-
-  return res.status(200).json(data)
 }
